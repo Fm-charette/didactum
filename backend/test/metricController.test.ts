e35144002ab1c9085e472614dd95f18025c39a71
Floryan MOLLET <floryan@kanope.io>
2025-03-22 15:46:47 +0100

    Feat: Install jest ts. Add one test, check if error code and message are equal to error code and message in metricsController.

---
+++ backend/test/metricController.test.ts
@@ -0,0 +1,23 @@
+import { MetricsController } from '../src/metrics.controller';
+import { HttpError } from 'routing-controllers';
+
+jest.mock('../src/database', () => ({
+  db: {
+    selectFrom: jest.fn().mockReturnThis(),
+    selectAll: jest.fn().mockReturnThis(),
+    limit: jest.fn().mockReturnThis(),
+    offset: jest.fn().mockReturnThis(),
+    execute: jest.fn().mockRejectedValue(new Error('Database error')),
+  },
+}));
+
+describe('MetricsController', () => {
+  let metricsController: MetricsController;
+  beforeEach(() => {
+    metricsController = new MetricsController();
+  });
+  it('return 500 and the error message when database fails', async () => {
+    const expectedError = new HttpError(500, "Une erreur est survenue lors de la récupération des métriques.");
+    await expect(metricsController.getAll()).rejects.toThrow(expectedError);
+  });
+});
